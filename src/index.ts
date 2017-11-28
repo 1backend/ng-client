06c1a998d4be424db5cf20b52cd54ba82f4d12cb
Janos Dobronszki <dobronszki@gmail.com>
2017-11-28 18:51:21 +0100

    Init

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+@Injectable()
+export class NgClient {
+  public token: string;
+  public address = 'https://1backend.com:9993';
+  constructor(private http: HttpClient) {}
+
+  // Call enables you to access any endpoint of any service on 1Backend
+  call<R>(
+    author: string,
+    projectName: string,
+    method: string,
+    path: string,
+    input: Map<string, any>
+  ): Promise<R> {
+    return new Promise<R>((resolve, reject) => {
+      let params = new HttpParams();
+      input.forEach((value, key) => {
+        params = params.set(key, value);
+      })
+      if (method.toLowerCase() === 'get') {
+        this.http.get<R>(this.address + path, {params: params}).subscribe((data: R) => {
+          resolve(data);
+        }, error => {
+          reject(error);
+        });
+      }
+      if (method.toLowerCase() === 'post') {
+        this.http.post<R>(this.address + path, input).subscribe((data: R) => {
+          resolve(data);
+        }, error => {
+          reject(error);
+        });
+      }
+      if (method.toLowerCase() === 'put') {
+        this.http.put<R>(this.address + path, input).subscribe((data: R) => {
+          resolve(data);
+        }, error => {
+          reject(error);
+        });
+      }
+      if (method.toLowerCase() === 'get') {
+        this.http.delete<R>(this.address + path, {params: params}).subscribe((data: R) => {
+          resolve(data);
+        }, error => {
+          reject(error);
+        });
+      }
+    });
+  }
+}
